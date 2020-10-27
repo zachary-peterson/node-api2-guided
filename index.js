1f72843ee9ce7be56993ef5c695aa146310bbb1f
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-10-27 18:24:26 +0100

    wip

--- index.js
+++ index.js
@@ -123,6 +123,7 @@ server.get('/api/hubs/:id/messages', (req, res) => {
 })
 // add an endpoint for adding new message to a hub [POST] { sender, text, hub_id }
 
+
 server.listen(4000, () => {
   console.log('\n*** Server Running on http://localhost:4000 ***\n');
 });
